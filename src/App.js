7d536380595fa629047488bccc80f6b95e8c482a
FarayW <287787880+FarayW@users.noreply.github.com>
2026-07-28 13:00:11 +0400

    ClassCounter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Counter from "./components/Counter";
 import TextInput from "./components/TextInput";
+import ClassCounter from "./components/ClassCounter";
 
 function App() {
     return (<div className="App">
@@ -8,6 +9,7 @@ function App() {
         <Counter/>
         <TextInput/>
         <TextInput/>
+        <ClassCounter/>
     </div>);
 }
 
