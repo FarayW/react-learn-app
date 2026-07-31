0947d034af619aff622c761e61c837a19047f490
FarayW <287787880+FarayW@users.noreply.github.com>
2026-07-31 16:53:26 +0400

    more posts!

--- src/App.js
+++ src/App.js
@@ -7,35 +7,33 @@ import TextInput from "./components/TextInput";
 import PostItem from "./components/PostItem";
 
 function App() {
+
+    const [posts, setPosts] = React.useState([
+        {id: 1, title: 'TypeScript', content: 'DESCRIPTION'},
+        {id: 2, title: 'TypeScript 2', content: 'DESCRIPTION'},
+        {id: 3, title: 'TypeScript 3', content: 'DESCRIPTION'},
+    ]);
+
+    const [posts2, setPosts2] = React.useState([
+        {id: 1, title: 'Rust', content: 'DESCRIPTION'},
+        {id: 2, title: 'Rust 2', content: 'DESCRIPTION'},
+        {id: 3, title: 'Rust 3', content: 'DESCRIPTION'},
+    ]);
+
+
+
     return (<div className="App">
-        <Counter/>
-        <ClassCounter/>
-        <CounterMult/>
-        <TextInput/>
-        <PostItem post={{
-            id: 1,
-            title: "Test Post",
-            content: "Test Post",
-        }}/>
-        <PostItem post={{
-            id: 2,
-            title: "Test FWQgfe",
-            content: "Test Post",
-        }}/>
-        <PostItem post={{
-            id: 3,
-            title: "Test ffAQWasFF",
-            content: "TsaADSAest Post",
-        }}/>
+        {
+            posts.map((post) => (
+                <PostItem key={post.id} post={post} />
+            ))
+        }
 
-        <PostItem post={{
-            id: 4,
-            title: "Test dada",
-            content: "eqwadqe Post",
-        }}/>
-
-
-
+        {
+            posts2.map((post) => (
+                <PostItem key={post.id} post={post} />
+            ))
+        }
     </div>);
 }
 
